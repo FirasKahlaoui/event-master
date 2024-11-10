3242eeb005283a5738a09b14a755219e99428374
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-10 10:07:54 +0100

    Insert events

--- src/firebase/insertEvents.js
+++ src/firebase/insertEvents.js
@@ -1,24 +1,32 @@
-// firebase/insertEvents.js
 const { db } = require("./admin");
 
 const events = [
   {
-    title: "Event 1",
-    description: "Description for Event 1",
-    date: "2023-10-01",
-    location: "Location 1",
+    title: "Bowling Tournament",
+    shortDescription: "Join our exciting bowling tournament!",
+    description:
+      "Get ready to compete in an intense bowling tournament at La Marsa. This event is open for all skill levels, offering an exciting opportunity to test your bowling skills, make new friends, and enjoy a fun atmosphere. Prizes await the top performers!",
+    date: "2024-11-30",
+    location: "La Marsa, Tunisia",
+    image: "/assets/images/bowling.avif",
   },
   {
-    title: "Event 2",
-    description: "Description for Event 2",
-    date: "2023-10-02",
-    location: "Location 2",
+    title: "Live Concert Night",
+    shortDescription: "An unforgettable night of live music!",
+    description:
+      "Experience an amazing evening with live performances by popular artists at Gammarth. Enjoy a diverse lineup of music genres, from jazz to pop, and indulge in the vibrant energy of the crowd. Don't miss out on this musical extravaganza!",
+    date: "2024-12-15",
+    location: "Gammarth, Tunisia",
+    image: "/assets/images/concert.webp",
   },
   {
-    title: "Event 3",
-    description: "Description for Event 3",
-    date: "2023-10-03",
-    location: "Location 3",
+    title: "Billiards Championship",
+    shortDescription: "Test your skills at the billiards championship!",
+    description:
+      "Join us at Lac 2 for a thrilling billiards championship. Whether you're a pro or just love the game, this competition will offer a chance to showcase your skills, meet fellow enthusiasts, and compete for the championship title!",
+    date: "2024-12-20",
+    location: "Lac 2, Tunisia",
+    image: "/assets/images/billiards.webp",
   },
 ];
 
