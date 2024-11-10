df5f04e1f21efe5302502ac93baededdc8cb6d85
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-10 09:40:44 +0100

    From details to the login

--- src/components/eventlist/index.jsx
+++ src/components/eventlist/index.jsx
@@ -1,31 +1,54 @@
-// EventList.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './EventList.css';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext";
+import "./EventList.css";
 
 const EventList = ({ events }) => {
-    return (
-        <section className="event-list">
-            <h2>Upcoming Events</h2>
-            <div className="event-list-container">
-                {events.length > 0 ? (
-                    events.map((event) => (
-                        <div className="event-item" key={event.id}>
-                            <img src={event.thumbnail || '/assets/default-event.jpg'} alt={event.name} className="event-thumbnail" />
-                            <div className="event-details">
-                                <h3>{event.name}</h3>
-                                <p>{event.description}</p>
-                                <p className="event-date">{new Date(event.date).toLocaleDateString()}</p>
-                                <Link to={`/event/${event.id}`} className="event-link">View Details</Link>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <p>No upcoming events available.</p>
-                )}
+  const { userLoggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  const handleViewDetails = (eventId) => {
+    if (userLoggedIn) {
+      navigate(`/event/${eventId}`);
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <section className="event-list">
+      <h2>Upcoming Events</h2>
+      <div className="event-list-container">
+        {events.length > 0 ? (
+          events.map((event) => (
+            <div className="event-item" key={event.id}>
+              <img
+                src={event.thumbnail || "/assets/default-event.jpg"}
+                alt={event.name}
+                className="event-thumbnail"
+              />
+              <div className="event-details">
+                <h3>{event.name}</h3>
+                <p>{event.description}</p>
+                <p className="event-date">
+                  {new Date(event.date).toLocaleDateString()}
+                </p>
+                <button
+                  onClick={() => handleViewDetails(event.id)}
+                  className="event-link"
+                >
+                  View Details
+                </button>
+              </div>
             </div>
-        </section>
-    );
+          ))
+        ) : (
+          <p>No upcoming events available.</p>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default EventList;
