3e2b522633019ba12dcf690cf09472912779fe0e
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-20 02:36:28 +0100

    Update the Cloudanary Helper

--- src/components/utils/cloudinaryHelper.js
+++ src/components/utils/cloudinaryHelper.js
@@ -1,29 +1,32 @@
-import { v2 as cloudinary } from "cloudinary";
+// src/components/utils/cloudinaryHelper.js
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// Cloudinary configuration
-cloudinary.config({
-  cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.REACT_APP_CLOUDINARY_API_KEY,
-  api_secret: process.env.REACT_APP_CLOUDINARY_API_SECRET,
-});
+const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`;
 
 /**
  * Uploads an image to Cloudinary and returns the URL of the uploaded image.
  * @param {File} imageFile - The image file to be uploaded.
- * @param {string} publicId - (Optional) A custom public ID for the uploaded image in Cloudinary.
  * @returns {Promise<string>} - The URL of the uploaded image.
  */
-export const uploadImage = async (imageFile, publicId = null) => {
+export const uploadImage = async (imageFile) => {
+  const formData = new FormData();
+  formData.append("file", imageFile);
+  formData.append("upload_preset", "your_upload_preset"); // Replace with your upload preset
+
   try {
-    const options = publicId ? { public_id: publicId } : {};
-    const uploadResult = await cloudinary.uploader.upload(
-      imageFile.path,
-      options
-    );
-    return uploadResult.secure_url; // Return the Cloudinary URL of the uploaded image
+    const response = await fetch(cloudinaryUrl, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to upload image");
+    }
+
+    const data = await response.json();
+    return data.secure_url; // Return the Cloudinary URL of the uploaded image
   } catch (error) {
     console.error("Error uploading image:", error);
     throw error;
