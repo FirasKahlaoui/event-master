0bd5ac4fd13e89b0123f97e172226ab6a2ecb538
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-22 18:42:04 +0100

    Comment the eventdetails

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { AuthProvider } from "./contexts/authContext";
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import Home from "./components/home";
-import EventDetails from "./components/eventdetails";
+/*import EventDetails from "./components/eventdetails";*/
 import SelectTopics from "./components/selecttopics";
 import MyEvents from "./components/myevents";
 import CreateEvent from "./components/createevent";
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/event/:id" element={<EventDetails />} />
+          {/*<Route path="/event/:id" element={<EventDetails />} />*/}
           <Route path="/select-topics" element={<SelectTopics />} />
           <Route path="/my-events" element={<MyEvents />} />
           <Route path="/create-event" element={<CreateEvent />} />
