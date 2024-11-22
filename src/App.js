db25f73ac742f21310c2da4319048ba9248832de
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-22 19:06:32 +0100

    Update the routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import Home from "./components/home";
 /*import EventDetails from "./components/eventdetails";*/
+import EventsDetails from "./components/eventsdetails";
 import SelectTopics from "./components/selecttopics";
 import MyEvents from "./components/myevents";
 import CreateEvent from "./components/createevent";
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           {/*<Route path="/event/:id" element={<EventDetails />} />*/}
+          <Route path="/event/:id" element={<EventsDetails />} />
           <Route path="/select-topics" element={<SelectTopics />} />
           <Route path="/my-events" element={<MyEvents />} />
           <Route path="/create-event" element={<CreateEvent />} />
