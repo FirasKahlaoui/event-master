6189f379a060d1e09a0f54d238fff13683c3b5de
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-12-06 18:33:47 +0100

    Update the routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/authContext";
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
+import AdminLogin from "./components/auth/adminlogin";
 import Home from "./components/home";
 import EventsDetails from "./components/eventsdetails";
 import SelectTopics from "./components/selecttopics";
@@ -11,6 +12,7 @@ import CreateEvent from "./components/createevent";
 import ManageEvent from "./components/manageevent";
 import Explore from "./components/explore";
 import Profile from "./components/profile";
+import AdminDashboard from "./components/admindashboard";
 import TwoFactorSetup from "./components/auth/TwoFactorSetup";
 
 const App = () => {
@@ -20,6 +22,7 @@ const App = () => {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/home" element={<Home />} />
           <Route path="/event/:id" element={<EventsDetails />} />
           <Route path="/select-topics" element={<SelectTopics />} />
@@ -28,6 +31,7 @@ const App = () => {
           <Route path="/manage-event/:id" element={<ManageEvent />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/two-factor-setup" element={<TwoFactorSetup />} />
           <Route path="/" element={<Home />} />
         </Routes>
