e5e9e7d8baf171c4b372ac9601bd802626998357
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-12-18 19:49:11 +0100

    Redirection to the admin-login

--- src/components/admindashboard/index.jsx
+++ src/components/admindashboard/index.jsx
@@ -1,70 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
-import { db } from "../../firebase/firebase";
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { useAuth } from "../../contexts/authContext";
-import Navbar from "../navbar";
-import "./AdminDashboard.css";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
-const AdminDashboard = () => {
+const ProtectedRoute = ({ children }) => {
   const { currentUser } = useAuth();
-  const [events, setEvents] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
-  useEffect(() => {
+  React.useEffect(() => {
     const checkAdminStatus = async () => {
       if (currentUser) {
         const adminDoc = await getDoc(doc(db, "admin", currentUser.uid));
         setIsAdmin(adminDoc.exists());
       }
+      setLoading(false);
     };
-
-    const fetchEvents = async () => {
-      const eventsCollection = collection(db, "events");
-      const eventsSnapshot = await getDocs(eventsCollection);
-      setEvents(eventsSnapshot.docs.map((doc) => doc.data()));
-    };
-
-    const fetchUsers = async () => {
-      const usersCollection = collection(db, "users");
-      const usersSnapshot = await getDocs(usersCollection);
-      setUsers(usersSnapshot.docs.map((doc) => doc.data()));
-    };
-
     checkAdminStatus();
-    fetchEvents();
-    fetchUsers();
   }, [currentUser]);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   if (!currentUser) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/admin-login" replace />;
   }
 
   if (!isAdmin) {
     return <Navigate to="/home" replace />;
   }
 
-  return (
-    <div>
-      <Navbar />
-      <div className="admin-dashboard-container">
-        <h1>Admin Dashboard</h1>
-        <div className="kpi-container">
-          <div className="kpi">
-            <h2>Total Events</h2>
-            <p>{events.length}</p>
-          </div>
-          <div className="kpi">
-            <h2>Total Users</h2>
-            <p>{users.length}</p>
-          </div>
-          {/* Add more KPIs as needed */}
-        </div>
-        {/* Add more visualizations and data as needed */}
-      </div>
-    </div>
-  );
+  return children;
 };
 
-export default AdminDashboard;
+export default ProtectedRoute;
